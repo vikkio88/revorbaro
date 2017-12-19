36c02b87874e19852600e31afd56e26bf7c3a765
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2017-12-19 19:00:24 +0000

    counting rounds and games

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import './App.css';
 import { Commands, Bullet, LastAction } from './components';
+import './App.css';
+import restart from './assets/next.svg';
 
 const INITIAL_STATE = {
   winner: null,
@@ -16,21 +17,44 @@ const INITIAL_STATE = {
 
 class App extends Component {
   state = {
-    winner: null,
-    player: {
-      loaded: false
-    },
-    computer: {
-      loaded: false
+    games: 0,
+    rounds: 1,
+    won: 0,
+    lost: 0,
+    context: {
+      winner: null,
+      player: {
+        loaded: false
+      },
+      computer: {
+        loaded: false
+      }
     }
   };
 
+  resetGame() {
+    let { won, lost, games, context } = this.state;
+    games += 1;
+    if (context.winner === "player") {
+      won += 1;
+    } else {
+      lost += 1;
+    }
+
+    this.setState({
+      context: INITIAL_STATE,
+      games,
+      rounds: 1,
+      won,
+      lost
+    });
+  }
   postAction(newState) {
-    this.setState(newState);
+    this.setState({ context: newState, rounds: this.state.rounds + 1 });
   }
 
   render() {
-    const { player, computer, winner } = this.state;
+    const { player, computer, winner } = this.state.context;
     return (
       <div className="App">
         <div
@@ -49,6 +73,12 @@ class App extends Component {
           <Bullet loaded={computer.loaded} />
           <LastAction action={computer.lastAction} />
         </div>
+        <div className="round-counter">
+          <h1>Round: {this.state.rounds}</h1>
+          <h3>Games Played: {this.state.games}</h3>
+          <strong>Games Won: {this.state.won}</strong>
+          <strong>Games Lost: {this.state.lost}</strong>
+        </div>
         <div className="controls">
           {
             !winner &&
@@ -56,9 +86,12 @@ class App extends Component {
           }
 
           {winner && (
-            <div>
-              <strong>{winner.toUpperCase()} Won</strong>
-              <button onClick={() => this.setState(INITIAL_STATE)}>Restart</button>
+            <div className="winner">
+              <h1>{winner.toUpperCase()} Won</h1>
+              <button className="action-button" onClick={() => this.resetGame()}>
+                <img className="icon" src={restart} alt="Restart" />
+                <span>Restart</span>
+              </button>
             </div>
           )}
         </div>
